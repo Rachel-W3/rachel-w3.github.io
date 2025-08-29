0951179de7eb6ee8c34a321ee88f7131235e031b
Rachel <rwong3@stuy.edu>
2025-08-28 20:05:41 -0400

    chore: whitespace changes

--- react-app/src/navbar/Navbar.tsx
+++ react-app/src/navbar/Navbar.tsx
@@ -6,22 +6,22 @@ function Navbar() {
   return (
     <div className="Navbar">
       <nav className="navbar navbar-expand-md navbar-dark navbar-override">
-            <h1 id="name">Rachel Wong</h1>
-            <button className="navbar-toggler m-0" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <h1 id="name">Rachel Wong</h1>
+        <button className="navbar-toggler m-0" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
             
-            <div className="collapse navbar-collapse" id="navbarsExample04">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link nav-color-override" href="#projects">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link nav-color-override" href="WongRachel_Resume.pdf" target="_blank">Resume</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className="collapse navbar-collapse" id="navbarsExample04">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link nav-color-override" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-color-override" href="WongRachel_Resume.pdf" target="_blank">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
